e2910f0551f8f803c99748a32bf7585de6081010
adolfo.alvarez <adolfo.alvarez@olx.pl>
2021-08-31 16:42:49 +0200

    Removing old version of my website

--- Gemfile
+++
@@ -1,9 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'jekyll'
-gem 'jekyll-sitemap'
-gem 'stringex'
-
-group :test do
-  gem 'html-proofer'
-end
--- code/users.R
+++
@@ -1,4 +0,0 @@
-# This code generates the choropleth map of R users meetup groups
-library(rvest)
-library(dplyr)
-r_users <- read_html("http://r-users-group.meetup.com/all/")
